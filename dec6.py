e8f48902e7cbfb665910f7be468c0eaea786d5c2
Mohan <mohan.agr97@gmail.com>
2022-12-06 11:28:40 +0530

    dec6

---
+++ dec6.py
@@ -0,0 +1,25 @@
+with open('dec6input.txt') as f:
+    x=f.read().strip().split('\n')[0]
+L=14
+for i in range(len(x)-L-1):
+    if(len(set(x[i:i+L]))==L):
+        print("GOT", i+L)
+        break
+
+# non-set solution
+# for i in range(len(x)-13):
+#     flag=1
+#     substr = x[i:i+14]
+#     substr=sorted(substr)
+#     for j in range(len(substr)-1):
+#         if(substr[j]==substr[j+1]):
+#             flag=0
+#             break
+#     if(flag):
+#         print(substr, "GOT", i+14)
+#         break
+
+
+
+
+    
